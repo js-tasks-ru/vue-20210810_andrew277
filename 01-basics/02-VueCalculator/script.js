bdbb865c0899c72015e673706a2759e8a4e80e95
AndrewBeznosko <andrew.beznosko@gmail.com>
2021-08-24 19:49:03 +0300

    ADD: VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,31 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const RootComponent = defineComponent({
+  data() {
+    return {
+      numA: 0,
+      numB: 0,
+      radio: null,
+    };
+  },
+  computed: {
+    result() {
+      switch (this.radio) {
+        case 'sum':
+          return this.numA + this.numB;
+        case 'subtract':
+          return this.numA - this.numB;
+        case 'multiply':
+          return this.numA + this.numB;
+        case 'divide':
+          return this.numA / this.numB;
+        default:
+          return 0;
+      }
+    },
+  },
+});
+
+const app = createApp(RootComponent);
+
+app.mount('#app');

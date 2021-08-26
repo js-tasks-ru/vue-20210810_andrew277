8e3ffcdc3bee1639e1211cb8447ba2317096d635
AndrewBeznosko <andrew.beznosko@gmail.com>
2021-08-26 23:05:34 +0300

    ADD: MeetupInfo

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,56 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  computed: {
+    dateFormattedLong() {
+      return this.formatDate({
+        options: {
+          day: 'numeric',
+          month: 'long',
+          year: 'numeric',
+        },
+      });
+    },
+    dateFormattedShort() {
+      return this.formatDate({ locale: 'en-CA' });
+    },
+  },
+  methods: {
+    formatDate({ locale = navigator.language, options = {} }) {
+      const date = new Date(this.date);
+
+      return date.toLocaleDateString(locale, options);
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ this.organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ this.place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="this.dateFormattedShort">{{ this.dateFormattedLong }}</time>
       </li>
     </ul>`,
 });
